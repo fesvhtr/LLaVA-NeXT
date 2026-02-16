7548633b2acb33e36ac35fa024e294aebacd8e08
fesvhtr <fesvhtr@gmail.com>
2026-02-16 07:26:20 +0400

    fix: vision tower

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -14,7 +14,7 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, "mm_vision_tower", getattr(vision_tower_cfg, "vision_tower", None))
     vision_tower_lower = vision_tower.lower()
     use_s2 = getattr(vision_tower_cfg, "s2", False)
-    if vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
+    if vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower or "clip" in vision_tower_lower:
         if use_s2:
             return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
         else:
